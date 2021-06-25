18c5e9ae359dd287eee30518c3e04201abeb4343
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-25 17:44:50 +0530

    Navbar and Footer with css

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./Footer.module.css";
+
+const Footer = () => <div className={styles.footer}>Error 404</div>;
+
+export default Footer;
---
+++ frontend/src/Components/Navbar/Navbar2.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+
+import styles from "./Navbar2.module.css";
+import { NavLink } from "react-router-dom";
+
+import Logo from "../../images/logo.png";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <ul className={styles.main_nav}>
+          <li>
+            <NavLink
+              className={styles.log}
+              exact
+              to="/"
+              activeClassName={styles.activel}
+            >
+              <img src={Logo} className={styles.logo}></img>
+              <div className={styles.name}>
+                Mobi<span className={styles.vax}>Vax</span>
+              </div>
+            </NavLink>
+          </li>
+
+          <li className={styles.link}>
+            {this.props.admin ? (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/admin"
+                activeClassName={styles.active}
+              >
+                Patients
+              </NavLink>
+            ) : (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/"
+                activeClassName={styles.active}
+              >
+                Home
+              </NavLink>
+            )}
+          </li>
+
+          <li className={styles.link}>
+            {this.props.admin ? (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/directions"
+                activeClassName={styles.active}
+              >
+                Directions
+              </NavLink>
+            ) : (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/support"
+                activeClassName={styles.active}
+              >
+                Support
+              </NavLink>
+            )}
+          </li>
+
+          <li className={styles.link}>
+            {this.props.admin ? (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/chat"
+                activeClassName={styles.active}
+              >
+                Chat
+              </NavLink>
+            ) : (
+              <NavLink
+                className={styles.tag}
+                exact
+                to="/chat"
+                activeClassName={styles.active}
+              >
+                Chat
+              </NavLink>
+            )}
+          </li>
+
+          <li className={`${styles.link} ${styles.push}`}>
+            <NavLink
+              className={styles.tag}
+              exact
+              to="/auth"
+              activeClassName={styles.active}
+            >
+              Logout
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
