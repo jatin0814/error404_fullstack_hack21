0b1c4fdb51e683a4dea406a666ba55ce739e47d8
sidharth <201952234@iiitvadodara.ac.in>
2021-06-25 23:46:28 +0530

    added redux thunk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,16 @@ import React, {Component} from 'react';
 import {Route,Switch} from 'react-router-dom';
 // import {connect} from 'react-redux'
 
+
+import User from './Container/Auth/Public/Public';
 import Home from './Container/Home/Home';
+import Admin from './Container/Admin/Admin'
 
 class App extends Component{
+
+  // componentDidMount(){
+  //   this.props.onTryAutoSignup();
+  // }
 
   render(){
 
@@ -13,6 +20,8 @@ class App extends Component{
       <div>
         <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/auth' exact component={User}/>
+        <Route path='/admin' exact component={Admin}/>
         </Switch>
       </div>
     )
