54dc082e9822af2817306b1a58e6dbdb7cdb6d0c
sidharth <201952234@iiitvadodara.ac.in>
2021-06-25 23:48:01 +0530

    store restructured

---
+++ frontend/src/Store/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
+
+export const AUTH_SUCCESS_ADMIN = 'AUTH_SUCCESS_ADMIN';
+
+export const VERIFY_START = 'VERIFY_START';
---
+++ frontend/src/Store/actions/auth.js
@@ -0,0 +1,109 @@
+import axios from "axios";
+
+import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  console.log("start");
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId,
+  };
+};
+
+export const authSuccessAdmin = () => {
+  return {
+    type: actionTypes.AUTH_SUCCESS_ADMIN
+  };
+};
+
+export const authFail = (error) => {
+  console.log("aaa");
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error,
+  };
+};
+
+export const verifyStart = () => {
+  return {
+    type: actionTypes.VERIFY_START,
+  };
+};
+
+export const auth = (mobileNo) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      mobileNo: mobileNo,
+    };
+    let url = "http://localhost:9000/user/login";
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("phone", mobileNo);
+        dispatch(verifyStart());
+      })
+      .catch((e) => {
+        dispatch(authFail(e.message));
+        console.log(e);
+      });
+  };
+};
+
+
+export const adminAuth = (username, password) => {
+  console.log(username, password)
+  console.log("adminAuth")
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      username,
+      password
+    };
+    let url = "http://localhost:9000/van/vanlogin";
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        // localStorage.setItem("phone", mobileNo);
+        dispatch(authSuccessAdmin());
+      })
+      .catch((e) => {
+        dispatch(authFail(e.message));
+        console.log(e);
+      });
+  };
+};
+
+
+export const verify = (mobileNo, otp) => {
+  return (dispatch) => {
+    dispatch(authStart());
+    const authData = {
+      mobileNo,
+      otp,
+    };
+    let url = "http://localhost:9000/user/verify";
+
+    axios
+      .post(url, authData)
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("userId", response.data.userId);
+        dispatch(authSuccess());
+      })
+      .catch((e) => {
+        dispatch(authFail(e));
+        console.log(e.message);
+      });
+  };
+};
