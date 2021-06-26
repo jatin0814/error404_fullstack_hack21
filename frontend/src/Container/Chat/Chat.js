cb52e856af0b4d603fbbb6db89c490b127f1d1ba
Aditya Singh <201952202@iiitvadodara.ac.in>
2021-06-27 02:02:23 +0530

    Chat System

--- frontend/src/Container/Chat/Chat.js
+++ frontend/src/Container/Chat/Chat.js
@@ -1,214 +1,214 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import styles from './Chat.module.css'
+import styles from "./Chat.module.css";
 // import Header from '../../components/Header/Header'
-import socket from '../../Components/socket'
-
+import socket from "../../Components/socket";
 
 class Chat extends Component {
+  state = {
+    mssg: "",
+    messages: [],
+    room: "Support System",
+    users: [],
+  };
 
-    state = {
-        mssg: "",
-        messages: [],
-        room: "aa",
-        users: []
-    }
+  onInputChangeHandler = (event) => {
+    this.setState({ mssg: event.target.value });
+  };
 
-    onInputChangeHandler = (event) => {
-        this.setState({mssg: event.target.value})
-    }
+  autoScroll = () => {
+    const mssgs = document.getElementById("mssgs");
 
-    autoScroll = () => {
+    mssgs.scrollTop = mssgs.scrollHeight;
+  };
 
-      const mssgs = document.getElementById("mssgs")
+  onSendMessage = (event) => {
+    var today = new Date(),
+      time = today.getHours() + ":" + today.getMinutes();
 
-      mssgs.scrollTop = mssgs.scrollHeight;
+    const user = localStorage.getItem("phone");
 
-    }
+    event.preventDefault();
 
-    onSendMessage = (event) => {
+    socket.emit("sendMessage", this.state.mssg, time, user, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("message delivered!");
+        this.setState({ mssg: "" });
+        this.nameInput.focus();
+      }
+    });
 
-        var today = new Date(),
-        time = today.getHours() + ':' + today.getMinutes();
+    this.autoScroll();
+  };
 
-        const user = localStorage.getItem("phone")
+  onSendAEFI = (event) => {
+    var today = new Date(),
+      time = today.getHours() + ":" + today.getMinutes();
 
-        event.preventDefault();
+    const user = localStorage.getItem("phone");
 
-        socket.emit('sendMessage', this.state.mssg, time, user, (err) => {
-           if(err) {
-               console.log(err)
-           }else{
-               console.log('message delivered!')
-               this.setState({mssg: ''})
-               this.nameInput.focus();
-           }
-        });
+    event.preventDefault();
 
-        this.autoScroll();
-    }
-
-    onSendAEFI = (event) => {
-
-        var today = new Date(),
-        time = today.getHours() + ':' + today.getMinutes();
-
-        const user = localStorage.getItem("phone")
-
-        event.preventDefault();
-
-        socket.emit('sendMessage', "AEFI EMERGENCY! I need immidiate help.", time, user, (err) => {
-           if(err) {
-               console.log(err)
-           }else{
-               console.log('message delivered!')
-               this.setState({mssg: ''})
-               this.nameInput.focus();
-           }
-        });
-
-        this.autoScroll();
-    }
-
-    onSendLocation = (event) => {
-
-        var today = new Date(),
-        time = today.getHours() + ':' + today.getMinutes();
-
-        const user = localStorage.getItem("phone")
-
-        event.preventDefault();
-
-        if(!navigator.geolocation){
-            return alert("This feature is not supported on your browser!")
+    socket.emit(
+      "sendMessage",
+      "AEFI EMERGENCY! I need immidiate help.",
+      time,
+      user,
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("message delivered!");
+          this.setState({ mssg: "" });
+          this.nameInput.focus();
         }
+      }
+    );
 
-        navigator.geolocation.getCurrentPosition((position) => {
+    this.autoScroll();
+  };
 
-            socket.emit('sendLocation', {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            }, time, user, () => {
-                console.log('Location sent!')
-            });
-        })
+  onSendLocation = (event) => {
+    var today = new Date(),
+      time = today.getHours() + ":" + today.getMinutes();
 
-        this.autoScroll();
-       
+    const user = localStorage.getItem("phone");
+
+    event.preventDefault();
+
+    if (!navigator.geolocation) {
+      return alert("This feature is not supported on your browser!");
     }
 
-    componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      socket.emit(
+        "sendLocation",
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        },
+        time,
+        user,
+        () => {
+          console.log("Location sent!");
+        }
+      );
+    });
 
-        // socket.emit('join', localStorage.getItem("phone"), this.props.match.params.room, (e) => {
-        //     if(e){
-        //         alert(e);
-        //     }
-        // });
-    }
+    this.autoScroll();
+  };
 
-    render() {
+  componentDidMount() {
+    // socket.emit('join', localStorage.getItem("phone"), this.props.match.params.room, (e) => {
+    //     if(e){
+    //         alert(e);
+    //     }
+    // });
+  }
 
+  render() {
+    socket.off("connection").on("connection", (mssg) => {
+      socket.emit("sendMessage", mssg, "time", "Admin", (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          this.setState({ mssg: "" });
+        }
+      });
+    });
 
-        socket.off('connection').on('connection', (mssg) => {
-            socket.emit('sendMessage', mssg, "time", "Admin", (err) => {
-                if(err) {
-                    console.log(err)
-                }else{
-                    this.setState({mssg: ''})
-                }
-            });
-        });
+    socket.off("message").on("message", (mssg, d, user) => {
+      this.state.messages.push(
+        <div className={styles.mssg}>
+          <div className={styles.user}>{user}</div>
+          <div className={styles.time}>{d}</div>
+          <div className={styles.txt}>{mssg}</div>
+        </div>
+      );
+      this.setState({ message: "" });
+    });
 
-        socket.off('message').on('message', (mssg, d, user) => {
-            this.state.messages.push(
-            <div className={styles.mssg}>
-                <div className={styles.user}>{user}</div>
-                <div className={styles.time}>{d}</div>
-                <div className={styles.txt}>{mssg}</div> 
+    socket.off("locationMessage").on("locationMessage", (mssg, time, user) => {
+      this.state.messages.push(
+        <div className={styles.mssg}>
+          <div className={styles.user}>{user}</div>
+          <div className={styles.time}>{time}</div>
+          <a className={styles.txt_loc} href={mssg} target="_blank">
+            My Location <i class="fa fa-location-arrow"></i>
+          </a>
+        </div>
+      );
+      this.setState({ message: "" });
+      console.log(mssg);
+    });
+
+    socket.off("roomData").on("roomData", ({ room, users }) => {
+      console.log(room);
+      console.log(users);
+      this.setState({ room: room });
+
+      this.setState({ users: [] });
+
+      users.map((user) => {
+        this.state.users.push(<div>{user.username}</div>);
+      });
+
+      console.log(this.state.users);
+    });
+
+    return (
+      <div>
+        <div className={styles.chat}>
+          <div className={styles.side}>
+            <div className={styles.room}>{this.state.room}</div>
+            <div className={styles.users_list}>
+              Ask your Queries from Live Support System <br />
+              <br />
+              Press AEFI button in case of Emergency post Vaccination.
             </div>
-           )
-            this.setState({message : ""})           
-        });
+            <div className={styles.users_cont}>{this.state.users}</div>
+          </div>
 
-
-        socket.off('locationMessage').on('locationMessage', (mssg, time, user) => {
-        this.state.messages.push(
-            <div className={styles.mssg}>
-                <div className={styles.user}>{user}</div>
-                <div className={styles.time}>{time}</div>
-                <a className={styles.txt_loc} href={mssg} target="_blank">
-                    My Location <i class="fa fa-location-arrow"></i>
-                </a>
-            </div>
-       )
-        this.setState({message : ""})
-        console.log(mssg);
-        });
-
-
-        socket.off('roomData').on('roomData', ({room, users}) => {
-           
-            console.log(room)
-            console.log(users)
-            this.setState({room: room});
-
-            this.setState({users: []})
-            
-           users.map(user => {
-               this.state.users.push(<div>{user.username}</div>);
-           })
-
-           console.log(this.state.users);
-           
-        })
-
-        return(
-
-            <div>
-
-
-                <div className={styles.chat}>
-
-                    <div className={styles.side}>
-                        <div className={styles.room}>{this.state.room}</div>
-                        <div className={styles.users_list}>Users</div>
-                        <div className={styles.users_cont}>{this.state.users}</div>                        
-                    </div>
-                       
-                    <div>
-
-                        <div className={styles.main_chat}>
-
-                            <div className={styles.mssgs_cont} id="mssgs">
-                                <div className={styles.chat_messages}>{this.state.messages}</div>
-                            </div>
-
-                            <div className={styles.compose}>
-
-                                    <form onSubmit={this.onSendMessage}>
-                                        <input className={styles.inpu} autoFocus type="text" value={this.state.mssg} onChange={this.onInputChangeHandler} 
-                                                ref={(input) => { this.nameInput = input; }}
-                                                defaultValue="It will focus"/>
-                                        <button onClick={this.onSendMessage} className={styles.send}>
-                                        <i class="fa fa-arrow-circle-right"></i>
-                                        </button>
-                                        <button onClick={this.onSendLocation} className={styles.loc}>Send Location</button>
-                                        <button onClick={this.onSendAEFI} className={styles.loc}>AEFI</button>
-
-                                    </form>
-
-                            </div>
-                           
-                        </div>
-                       
-                    </div>
-
+          <div>
+            <div className={styles.main_chat}>
+              <div className={styles.mssgs_cont} id="mssgs">
+                <div className={styles.chat_messages}>
+                  {this.state.messages}
                 </div>
+              </div>
 
+              <div className={styles.compose}>
+                <form onSubmit={this.onSendMessage}>
+                  <input
+                    className={styles.inpu}
+                    autoFocus
+                    type="text"
+                    value={this.state.mssg}
+                    onChange={this.onInputChangeHandler}
+                    ref={(input) => {
+                      this.nameInput = input;
+                    }}
+                    defaultValue="It will focus"
+                  />
+                  <button onClick={this.onSendMessage} className={styles.send}>
+                    <i class="fa fa-arrow-circle-right"></i>
+                  </button>
+                  <button onClick={this.onSendLocation} className={styles.loc}>
+                    Send Location
+                  </button>
+                  <button onClick={this.onSendAEFI} className={styles.loc2}>
+                    AEFI
+                  </button>
+                </form>
+              </div>
             </div>
-
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Chat;
